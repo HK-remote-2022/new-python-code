71879822baf5202821cae509f73dba1ccaad55ad
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-09 11:13:25 +0700

    My new Python Code

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 """
 Ini adalah demo projek pertama dengan Python
+Semua sinteaksi dasar pemrograman terdiri dari
+1. Sekuensial : Langkah berurutan
+2. Percabangan : Langkah melompat jika kondisi terpenuhi
+3. Pengulangan : mengulang langkah yang sama berkali kali selama/sampai kondisi terpenuhi
 """
 
+# Sekuensial
 print("hello world")
 print("My Name is Hendra")
+print('Ibu Berkata,"pulang jangan malam malam ya"')
+print('hendra menjawab,"oke bu, Hendra pulang pagi ya"')
+print('ibu berkata, "bagus, mendingan ga usah pulang aja ya"')
+print('hendra menjawab,"oke bu, Hendra gak pulang ya"')
