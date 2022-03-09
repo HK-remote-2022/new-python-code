92189d7d0a91a9547339e71443f320478e9f7798
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-09 13:03:37 +0700

    My new Python Code

--- main.py
+++ main.py
@@ -9,7 +9,20 @@ Semua sinteaksi dasar pemrograman terdiri dari
 # Sekuensial
 print("hello world")
 print("My Name is Hendra")
-print('Ibu Berkata,"pulang jangan malam malam ya"')
+print('Ibu Berkata,"pulang jangan malam malam ya jangan lebih dari jam 9 malam"')
 print('hendra menjawab,"oke bu, Hendra pulang pagi ya"')
-print('ibu berkata, "bagus, mendingan ga usah pulang aja ya"')
-print('hendra menjawab,"oke bu, Hendra gak pulang ya"')
+print('ibu berkata, "bagus, mendingan ga usah pulang aja ya!!"')
+print('hendra menjawab,"iya deh bu, liat nanti aja ya"')
+
+#Percabangan
+
+jam_pulang_main = 8
+
+if jam_pulang_main < 9:
+    print("Hendra cek jam berapa sekarang, ternyata masih bisa pulang sebelum jam 9")
+    print("Hendra pulang kerumah ")
+else:
+    print("Hendra liat jam ternyata sudah lewat jam 9 malam")
+    print("Hendra tidak jadi pulang dan nginep dirumah teman sampai pagi")
+
+
