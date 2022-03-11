3af8d1047bf82d53c230b2b28d061aa5ef3cb0b9
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-11 11:07:35 +0700

    Perulangan Baca Buku

--- dasar_sitaksis_perulangan_baca_buku.py
+++ dasar_sitaksis_perulangan_baca_buku.py
@@ -3,3 +3,11 @@ Program Peprulangan Membaca Buku
 """
 jumlah_buku = 10
 print('Ibu Berkata "baca semua bukumu')
+
+jumlah_buku_yang_sudah_dibaca = 0
+print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+
+for jumlah_buku_yang_sudah_dibaca in range(1, 11):
+    print(f"buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca")
+
+print(f'Jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
