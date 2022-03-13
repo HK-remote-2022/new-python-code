5a66f83b01ebd992ba74af1f73fd557ee307273a
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-13 13:02:56 +0700

    Perulangan Baca Buku

--- dasar_sitaksis_perulangan_baca_buku_dengan_while_indefined_count.py
+++ dasar_sitaksis_perulangan_baca_buku_dengan_while_indefined_count.py
@@ -3,14 +3,21 @@ Program Perulangan Membaca Buku Dengan While Sampai Paham
 """
 jumlah_buku = 10
 print('Ibu Berkata "baca semua bukumu"')
+jumlah_baca = 0
 
-jumlah_buku_yang_sudah_dibaca = 0
-print(f"jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}")
+jumlah_paham = 0
+print(f"jumlah buku yang sudah dibaca {jumlah_paham}")
 
-while jumlah_buku_yang_sudah_dibaca < jumlah_buku:
-    jumlah_buku_yang_sudah_dibaca = jumlah_buku_yang_sudah_dibaca + 1
-    print(f'buku ke {jumlah_buku_yang_sudah_dibaca} sudah dibaca')
+while jumlah_baca < jumlah_buku * 2:
+    jumlah_baca = jumlah_baca + 1
+    if jumlah_paham == 9:
+        print(f"buku ke {jumlah_paham+ 1} belum paham")
+    else :
+        jumlah_paham = jumlah_paham + 1
+        print(f'buku ke {jumlah_paham} sudah dibaca dan dipahami')
 
-print(f'jumlah buku yang sudah dibaca {jumlah_buku_yang_sudah_dibaca}')
-
-print("total buku yang ada =", jumlah_buku)
+print(f'jumlah buku yang sudah dibaca dan dipahami {jumlah_paham}')
+if jumlah_paham == jumlah_buku:
+    print("Bu, semua buku sudah dibaca dan dipahami")
+else:
+    print(f"Bu tidak semua buku bisa dipahami. Budi hanya bisa memahami ({jumlah_paham} buku")
