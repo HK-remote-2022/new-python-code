124e62e64e842c075ea2d307e8f61daa12eb2e37
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-15 12:13:16 +0700

    Belajar List

--- dasar_sitaksis_tipe_data_list.py
+++ dasar_sitaksis_tipe_data_list.py
@@ -28,3 +28,28 @@ print('\ntambah 1 buku baru')
 daftar_buku.append("buku tembus kerja remot")
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
+
+print('\nClear List')
+daftar_buku.clear()
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nGanti element pertama')
+daftar_buku = ["Bulughul Maram", "Syarah Akidah Alusunnah Wal Jamaah", "Riyadus Sholihin", "Sirah Nabawiyah"]
+daftar_buku [0] = 'Bulughul Maram Jilid 2'
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nAmbil element Kedua')
+buku = daftar_buku.pop(0)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
+
+print('\nBuku yang diambil tadi')
+print(buku)
+
+print('\nPop')
+daftar_buku = ["Bulughul Maram", "Syarah Akidah Alusunnah Wal Jamaah", "Riyadus Sholihin", "Sirah Nabawiyah"]
+daftar_buku.pop(2)
+for i in range(0, len(daftar_buku)):
+    print(daftar_buku[i])
