cfb6abf3ef7821d3afc8424c5bc2bcb9674f472f
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-15 09:59:51 +0700

    Perulangan Baca Buku

---
+++ dasar_sitaksis_tipe_data_list.py
@@ -0,0 +1,13 @@
+"""
+List
+"""
+daftar_buku = ["Bulughul Maram", "Syarah Akidah Alusunnah Wal Jamaah", "Riyadus Sholihin"]
+print('tampilkan variable daftar buku')
+print(daftar_buku)
+
+print("proses semua dengan for in")
+for buku in daftar_buku:
+    print(buku)
+
+print("tampilkan isi daftar_buku pada indeks tertentu")
+print (daftar_buku[0])
