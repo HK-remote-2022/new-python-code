efb1910b2891aa5dc2881abaf8e77d689f74c6e8
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-17 12:30:07 +0700

    Belajar List Comprehension

--- dasar_sitaksis_tipe_data_list.py
+++ dasar_sitaksis_tipe_data_list.py
@@ -53,21 +53,3 @@ daftar_buku = ["Bulughul Maram", "Syarah Akidah Alusunnah Wal Jamaah", "Riyadus 
 daftar_buku.pop(2)
 for i in range(0, len(daftar_buku)):
     print(daftar_buku[i])
-
-print('\nPerintah Del')
-daftar_buku = ["Bulughul Maram", "Syarah Akidah Alusunnah Wal Jamaah", "Riyadus Sholihin", "Sirah Nabawiyah"]
-del daftar_buku[2]
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nPerintah Del dengan List Comprehension')
-daftar_buku = ["Bulughul Maram", "Syarah Akidah Alusunnah Wal Jamaah", "Riyadus Sholihin", "Sirah Nabawiyah"]
-del daftar_buku[0:12] #START:END
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
-
-print('\nPerintah Del dengan List Comprehension start')
-daftar_buku = ["Bulughul Maram", "Syarah Akidah Alusunnah Wal Jamaah", "Riyadus Sholihin", "Sirah Nabawiyah"]
-del daftar_buku[0::2] #START:END:STEP
-for i in range(0, len(daftar_buku)):
-    print(daftar_buku[i])
