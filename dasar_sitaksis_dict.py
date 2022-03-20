ccff20630a6b66bd47da9f02a26791b6c438b886
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-20 14:42:01 +0700

    Belajar Dict

---
+++ dasar_sitaksis_dict.py
@@ -0,0 +1,12 @@
+user = {
+    'id': 1,
+    'name': 'Leanne Graham',
+    'username': "Bret",
+    'email': 'Sincere@april.biz',
+}
+
+print(user)
+print(user['id'])
+print(user['name'])
+print(user['username'])
+print(user['email'])
