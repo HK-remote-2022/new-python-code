0a059d05c3ee77dc5075dcd38de5440148ee3130
Hendra Kusuma <hendrakusuma.vegas@gmail.com>
2022-03-20 15:22:24 +0700

    Belajar Dict , dict didalam dict

--- dasar_sitaksis_dict.py
+++ dasar_sitaksis_dict.py
@@ -1,13 +1,31 @@
+"""
+Dictionary didalam Dictionary / Json didalam Json
+"""
+
 user = {
-    'id': 1,
-    'name': 'Leanne Graham',
-    'username': "Bret",
-    'email': 'Sincere@april.biz',
+    "id": 1,
+    "name": "Leanne Graham",
+    "username": "Bret",
+    "email": "Sincere@april.biz",
+    "address": {
+        "street": "Kulas Light",
+        "suite": "Apt. 556",
+        "city": "Gwenborough",
+        "zipcode": "92998-3874",
+        "geo": {
+            "lat": "-37.3159",
+            "lng": "81.1496"
+        }
+    }
 }
 
 print(user)
-print(user['id'])
-print(user['name'])
-print(user['username'])
-print(user['email'])
-print ('hello world')
+print(user["id"])
+print(user["name"])
+print(user["username"])
+print(user["email"])
+print (user["address"])
+print (user["address"]["street"])
+print (user["address"]["geo"])
+print (user["address"]["geo"]["lat"])
+print (user["address"]["geo"]["lng"])
